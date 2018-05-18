657de687b8243a67c7e8a88498edbe87aecebec6
Matěj Týč <matyc@redhat.com>
2018-05-18 13:38:21 +0200

    Introduced quasi-getter/setter-based C wrapper.

--- test/unit/test.c
+++ test/unit/test.c
@@ -7,12 +7,12 @@
 
 const size_t MAX_LEN = 128;
 
-/*
-SWIDHandle swid_create_data();
-int swid_load_data(SWIDIOHandle io_handle, SWIDHandle swid_structure);
-int swid_save_data(SWIDIOHandle io_handle, SWIDHandle swid_structure);
-*/
 
+const char * str_name = "ACME RoadRunner Management Suite";
+const char * str_tagId = "com.acme.rms-ce-v4-1-5-0";
+const char * str_version = "1.2.3";
+const char * str_versionScheme = "semver";
+const char * str_xml_lang = "en";
 
 
 void create_io(void)
@@ -27,40 +27,31 @@ void create_xml(const char * backend)
 	char output_fname[MAX_LEN];
 	snprintf(output_fname, MAX_LEN, "%s-c.xml", backend);
 
-	CSWIDStruct swid = swid_get_empty_data();
-	CSWIDStruct swid_loaded;
+	SWIDHandle swid = swid_get_empty_data();
+	SWIDHandle swid_loaded = swid_get_empty_data();
 
-	swid.name = malloc(MAX_LEN);
-	strcpy(swid.name, "ACME RoadRunner Management Suite");
+	swid_set_name(swid, str_name);
+	swid_set_tagId(swid, str_tagId);
+	swid_set_version(swid, str_version);
+	swid_set_versionScheme(swid, str_versionScheme);
+	swid_set_xml_lang(swid, str_xml_lang);
 
-	swid.tagId = malloc(MAX_LEN);
-	strcpy(swid.tagId, "com.acme.rms-ce-v4-1-5-0");
-
-	swid.version = malloc(MAX_LEN);
-	strcpy(swid.version, "1.2.3");
-
-	swid.versionScheme = malloc(MAX_LEN);
-	strcpy(swid.versionScheme, "semver");
-
-	swid.xml_lang = malloc(MAX_LEN);
-	strcpy(swid.xml_lang, "en-us");
-
-	swid.type = SWID_TYPE_CORPUS;
+	swid_set_type(swid, SWID_TYPE_CORPUS);
 
 	SWIDIOHandle io = swid_create_io(backend);
-	swid_save_data(io, output_fname, & swid);
-	swid_load_data(io, output_fname, & swid_loaded);
+	swid_save_data(io, output_fname, swid);
+	swid_load_data(io, output_fname, swid_loaded);
 	swid_destroy_io(io);
 
-	TEST_CHECK(strncmp(swid.name, swid_loaded.name, MAX_LEN) == 0);
-	TEST_CHECK(strncmp(swid.tagId, swid_loaded.tagId, MAX_LEN) == 0);
-	TEST_CHECK(strncmp(swid.version, swid_loaded.version, MAX_LEN) == 0);
-	TEST_CHECK(strncmp(swid.versionScheme, swid_loaded.versionScheme, MAX_LEN) == 0);
-	TEST_CHECK(strncmp(swid.xml_lang, swid_loaded.xml_lang, MAX_LEN) == 0);
-	TEST_CHECK(swid.type == swid_loaded.type);
+	TEST_CHECK(strcmp(str_name, swid_get_name(swid_loaded)) == 0);
+	TEST_CHECK(strcmp(str_tagId, swid_get_tagId(swid_loaded)) == 0);
+	TEST_CHECK(strcmp(str_version, swid_get_version(swid_loaded)) == 0);
+	TEST_CHECK(strcmp(str_versionScheme, swid_get_versionScheme(swid_loaded)) == 0);
+	TEST_CHECK(strcmp(str_xml_lang, swid_get_xml_lang(swid_loaded)) == 0);
+	TEST_CHECK(SWID_TYPE_CORPUS == swid_get_type(swid_loaded));
 
-	swid_destroy_data(& swid_loaded);
-	swid_destroy_data(& swid);
+	swid_destroy_data(swid_loaded);
+	swid_destroy_data(swid);
 }
 
 
